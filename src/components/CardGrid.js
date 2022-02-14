5ace2d9daea6746c04eb8f311f16ee52eaec8302
MayuRuru <aruizrull@protonmail.com>
2022-02-14 11:19:09 +0100

    m

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,21 +1,28 @@
 import { Card } from "./Card";
-import { getAllPoke } from "../service";
+//import { getAllPoke } from "../service";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import { apiService } from "../Globalapi";
 
 export const CardGrid = () => {
   const [pokes, setPokes] = useState([]);
 
   useEffect(() => {
-    getAllPoke().then((response) => setPokes(response));
-  }, []);
+    axios({ apiService }).then((response) => setPokes(response.data));
+  }, [setPokes]);
 
-  return console.log(response);
-  /*
+  return (
     <ul>
       {pokes.map((poke) => (
         <Card poke={poke} key={poke.id} image={poke.url} />
       ))}
     </ul>
-    
-  );*/
+  );
 };
+
+/*
+ useEffect(() => {
+    getAllPoke().then((response) => setPokes(response));
+  }, [setPokes]);
+
+*/
--- src/service.js
+++ src/service.js
@@ -4,6 +4,7 @@ import axios from "axios";
 export async function getAllPoke() {
   return axios.get(apiServer).then((response) => response.data);
 }
+console.log(response);
 
 export async function getPokeById(id) {
   return axios.get(apiServer + id).then((response) => response.data);
