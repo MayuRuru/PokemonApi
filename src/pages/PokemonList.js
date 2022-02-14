b6aff5cdfee2df1371e5f5927869ad9ead2004e7
MayuRuru <aruizrull@protonmail.com>
2022-02-14 18:13:40 +0100

    m

--- src/pages/PokemonList.js
+++ src/pages/PokemonList.js
@@ -4,11 +4,7 @@ export default function PokemonList({ pokemon }) {
   return (
     <div>
       {pokemon.map((poke, key, name, image) => (
-        <Card
-          key={poke}
-          name={poke.name}
-          image={poke.sprites.other.dream_world.front_default}
-        />
+        <Card key={poke} name={poke.name} />
       ))}
     </div>
   );
