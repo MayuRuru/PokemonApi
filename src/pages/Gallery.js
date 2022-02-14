31a26ad1f2b60bd7f1495e457a546aeb4d1c40eb
MayuRuru <aruizrull@protonmail.com>
2022-02-14 10:58:36 +0100

    m

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,9 +1,10 @@
-//import { CardGrid } from "../components/CardGrid";
+import { CardGrid } from "../components/CardGrid";
 
 function Gallery() {
   return (
     <div>
       <h1>POKEMON LIST</h1>
+      <CardGrid />
     </div>
   );
 }
