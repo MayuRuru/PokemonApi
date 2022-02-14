f4ddcd1202a75b36cebdc70c1395b60630a51b5c
MayuRuru <aruizrull@protonmail.com>
2022-02-14 10:53:01 +0100

    m

--- src/service.js
+++ src/service.js
@@ -2,9 +2,9 @@ import apiServer from "./globalapi";
 import axios from "axios";
 
 export async function getAllPoke() {
-  return axios.get(apiServer + "/pokemon").then((res) => res.data);
+  return axios.get(apiServer).then((res) => res.data);
 }
 
 export async function getPokeById(id) {
-  return axios.get(apiServer + "/pokemon/" + id).then((res) => res.data);
+  return axios.get(apiServer + id).then((res) => res.data);
 }
