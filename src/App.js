91a7cf748132a4c1971c813955c03eb034f5dce8
MayuRuru <aruizrull@protonmail.com>
2022-02-14 17:09:10 +0100

    m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import react from "react";
 import PokemonList from "./pages/PokemonList";
-import {useState} from react;
+import PassPage from "./components/PassPage";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const [pokemon, setPokemon] = useState([])
+  const [actualPage, setActualPage] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPage, setNextPage] = useState();
+  const [previousPage, setPreviousPage] = useState();
 
-  return <PokemonList pokemon={pokemon}/>;
+  useEffect(() => {
+    setLoading(false);
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+      setPokemon(res.data.results.map((poke) => poke.name));
+      setNextPage(res.data.next);
+      setPreviousPage(res.data.previous);
+    });
+  }, [actualPage]);
+
+  function goNext() {
+    setActualPage(nextPage);
+  }
+  function goPrevious() {
+    setActualPage(previousPage);
+  }
+
+  if (loading) return "Loading...";
+
+  return (
+  <PokemonList pokemon={pokemon} />
+  <PassPage 
+  goNext={goNext} 
+  goPrevious={goPrevious} />
+  );
 }
 
 export default App;
