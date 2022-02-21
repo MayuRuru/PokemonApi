a6353820cf6d03458c591489af16f86cca7dcd29
MayuRuru <aruizrull@protonmail.com>
2022-02-21 10:35:50 +0100

    m

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css";
+import "./fonts/Pokemon-Hollow.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
